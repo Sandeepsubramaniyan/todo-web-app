ed06c0cde914bf72616271ad9760e35ccf22c889
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-11-20 10:29:40 +0530

    renamed completed field

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=100)
-    compeleted = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     
     #displays title in admin
     def __str__(self):
