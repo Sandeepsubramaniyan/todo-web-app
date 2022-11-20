ecda6d6c1370f4d666b66aea0c0d407813b6118a
sandeepsubramaniyan <sandeepkathiravan@gmail.com>
2022-11-20 12:10:52 +0530

    Modelserializer spelling mistake

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Todo
 
 #Converting the model instances to JSON to work with frontend
-class TodoSerializer(serializers.ModelSerializers):
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
